c507ff857949969a95078e8dd0364d8786dfa5ea
Kyounj2 <108376415+Kyounj2@users.noreply.github.com>
2022-09-14 19:20:12 +0900

    0914

--- Assets/YJ/Script/YJ_Propmachines.cs
+++ Assets/YJ/Script/YJ_Propmachines.cs
@@ -1,24 +1,49 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 // F키를 누르면 게이지를 채우고싶다
 public class YJ_Propmachines : MonoBehaviour
 {
-    
+    public GameObject gage;
+    Slider slider;
+
+    bool macineOn = false;
     void Start()
     {
-        
+        gage.SetActive(false);
+        slider = gage.GetComponent<Slider>();
     }
 
     
     void Update()
     {
-        
+        if(gage && Input.GetKeyDown(KeyCode.F))
+        {
+            macineOn = true;
+        }
+
+        if(macineOn)
+        {
+           slider.value += 0.03f * Time.deltaTime;
+        }
     }
 
-    public void OnmachinePlay()
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.layer == 31)
+        {
+            gage.SetActive(true);
+        }
+    }
 
+    private void OnTriggerExit(Collider other)
+    {
+        if(other.gameObject.layer == 31)
+        {
+            macineOn = false;
+            gage.SetActive(false);
+        }
     }
 }
