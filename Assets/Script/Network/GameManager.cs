f95c61a1f45ab0fc8d012a30a3fae0bc48191f59
tang-ka <gur0907@hanyang.ac.kr>
2022-09-17 15:27:09 +0900

    dfffd

---
+++ Assets/Script/Network/GameManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class GameManager : MonoBehaviourPunCallbacks
+{
+    public static GameManager instance;
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        // OnPhotonSerializeView 호출 빈도
+        PhotonNetwork.SerializationRate = 60;
+        // RPC 호출 빈도
+        PhotonNetwork.SendRate = 60;
+
+        // 플레이어를 생성한다.
+        PhotonNetwork.Instantiate("Player", transform.position, Quaternion.identity);
+    }
+
+    void Update()
+    {
+        
+    }
+}
